bbe989e2b6bdfe161a207d716be06ac233895bc8
Martin Thompson <mjpt777@gmail.com>
2013-08-28 12:10:56 +0100

    javadoc correction

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -5,7 +5,7 @@
 public interface Spaceship
 {
     /**
-     * Read the current position of the spaceship into the array of coordinates provided.
+     * Read the position of the spaceship into the array of coordinates provided.
      *
      * @param coordinates into which the x and y coordinates should be read.
      * @return the number of attempts made to read the current state.
@@ -13,7 +13,7 @@ public interface Spaceship
     int readPosition(final int[] coordinates);
 
     /**
-     * Move the current position of the spaceship by a delta to the x and y coordinates.
+     * Move the position of the spaceship by a delta to the x and y coordinates.
      *
      * @param xDelta delta by which the spaceship should be moved in the x-axis.
      * @param yDelta delta by which the spaceship should be moved in the y-axis.
