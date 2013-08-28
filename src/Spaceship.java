8854589aaa5e5298766945cbf2801f2cf41dbd86
Martin Thompson <mjpt777@gmail.com>
2013-08-28 12:05:06 +0100

    Added javadoc to the Spaceship interface.

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -1,6 +1,23 @@
+/**
+ * Interface to a concurrent representation of a ship that can move around a 2 dimensional space with
+ * updates and reads performed concurrently.
+ */
 public interface Spaceship
 {
+    /**
+     * Read the current position of the spaceship into the array of coordinates provided.
+     *
+     * @param coordinates into which the x and y coordinates should be read.
+     * @return the number of attempts made to read the current state.
+     */
     int readPosition(final int[] coordinates);
 
+    /**
+     * Move the current position of the space by a delta to the x and y coordinates.
+     *
+     * @param xDelta delta by which the spaceship should be moved in the x-axis.
+     * @param yDelta delta by which the spaceship should be moved in the y-axis.
+     * @return the number of attempts made to write the new coordinates.
+     */
     int move(final int xDelta, final int yDelta);
 }
